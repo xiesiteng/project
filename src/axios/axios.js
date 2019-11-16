c652ec66246adfc8215cc29b232921cc5494ea40
xiesiteng <goku_97@163.com>
2019-11-16 17:19:49 +0800

    update in company

---
+++ src/axios/axios.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+import store from "../store";
+
+axios.defaults.baseURL= 'http://ymm.molonglan.top'
+axios.defaults.headers.post['Content-Type'] = "application/json"
+// axios.defaults.withCredentials = true
+axios.defaults.headers.common['YMM-TK'] = store.state.token
+// Vue.prototype.$axios = axios
+
+
+
+// 添加请求拦截器
+axios.interceptors.request.use(config => {
+// 在发送请求之前做些什么
+//判断是否存在token，如果存在将每个页面header都添加token
+  if(store.state.token){
+    config.headers.common['YMM-TK'] = store.state.token
+  }
+  return config;
+}, error => {
+// 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+
+// http response 拦截器
+axios.interceptors.response.use(
+  response => {
+    // console.log(response.data.code)
+    if (response.data.code == 4001) {
+      // this.$store.commit('delToken');
+      localStorage.removeItem("token")
+      router.replace({
+        path: '/login/login',
+        query: {redirect: router.currentRoute.fullPath}//登录成功后跳入浏览的当前页面
+      })
+    }
+    return response;
+  },
+  error => {
+    console.log(error.data)
+    if (error.response) {
+      switch (error.response.status) {
+        case 4001:
+          this.$store.commit('delToken');
+          router.replace({
+            path: '/login/login',
+            query: {redirect: router.currentRoute.fullPath}//登录成功后跳入浏览的当前页面
+          })
+      }
+    }
+    return Promise.reject(error.response.data)
+  });
+
+export default axios;
--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import axios from 'axios'
+import axios from './axios/axios'
+// import axios from 'axios'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 import 'lib-flexible/flexible'
@@ -15,12 +16,6 @@ Vue.use(Vant)
 
 Vue.config.productionTip = false
 
-axios.defaults.baseURL= 'http://ymm.molonglan.top'
-axios.defaults.headers.post['Content-Type'] = "application/json"
-// axios.defaults.withCredentials = true
-axios.defaults.headers.common['YMM-TK'] = store.state.token
-// Vue.prototype.$axios = axios
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
@@ -29,52 +24,6 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
-
-
-// 添加请求拦截器
-axios.interceptors.request.use(config => {
-// 在发送请求之前做些什么
-//判断是否存在token，如果存在将每个页面header都添加token
-  if(store.state.token){
-    config.headers.common['YMM-TK'] = store.state.token
-  }
-  return config;
-}, error => {
-// 对请求错误做些什么
-  return Promise.reject(error);
-});
-
-
-
-
-// http response 拦截器
-axios.interceptors.response.use(
-  response => {
-    // console.log(response.data.code)
-    if (response.data.code == 4001) {
-      // this.$store.commit('delToken');
-      localStorage.removeItem("token")
-      router.replace({
-        path: '/login/login',
-        query: {redirect: router.currentRoute.fullPath}//登录成功后跳入浏览的当前页面
-      })
-    }
-    return response;
-  },
-  error => {
-    console.log(error.data)
-    if (error.response) {
-      switch (error.response.status) {
-        case 4001:
-          this.$store.commit('delToken');
-          router.replace({
-            path: '/login/login',
-            query: {redirect: router.currentRoute.fullPath}//登录成功后跳入浏览的当前页面
-          })
-      }
-    }
-    return Promise.reject(error.response.data)
-  });
 
 
 Vue.prototype.fmtTime = function (number, format) {
@@ -96,4 +45,7 @@ Vue.prototype.fmtTime = function (number, format) {
   format=format.indexOf('s')>-1?format.replace('s',s):format;
   return format;
 }
+
+
+
 
